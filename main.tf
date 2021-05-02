434c92b8c8097949357afc6cdc702a94c0fa6fc2
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-02 20:29:17 +0100

    adding web sg

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "nitheeshp"
-    workspaces { prefix = "vpc-" }
+    workspaces { prefix = "sg-" }
   }
 }
 
