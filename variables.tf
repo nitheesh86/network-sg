511191ba31add165bfee8d601c8a28fbd8052fdc
Nitheesha Poojary <nitheesh86@gmail.com>
2021-05-02 18:06:08 +0100

    adding web sg

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "env" {
+  description = "Name of enviournment"
+  type        = string
+  default     = ""
+}
